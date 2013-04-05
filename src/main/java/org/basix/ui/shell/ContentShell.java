4cd906b7473676713c02eddd1ef4a3dae428d646
elhakimz <el.hakimz@gmail.com>
2013-04-06 04:28:34 +0700

    removal

--- src/main/java/org/basix/ui/shell/ContentShell.java
+++
@@ -1,9 +0,0 @@
-package org.basix.ui.shell;
-
-/**
- * User: abilhakim
- * Date: 3/31/13
- * Time: 1:37 PM
- */
-public class ContentShell {
-}
--- src/main/webapp/scripts/modules/webtools/ChartConsole.groovy
+++
@@ -1,6 +0,0 @@
-
-/**
- * User: abilhakim
- * Date: 4/3/13
- * Time: 7:58 PM
- */
